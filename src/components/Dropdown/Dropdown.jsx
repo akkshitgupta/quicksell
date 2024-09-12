e842e125c637085b46388a26b88f6970b9194720
akkshitgupta <96991785+akkshitgupta@users.noreply.github.com>
2024-09-12 20:19:21 +0000

    feat: add display modal

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,11 +1,38 @@
+import { useState } from "react";
 import { DownIcon } from "../../assets";
 import "./Dropdown.css";
 
 export default function ComponentName({ children }) {
+  const [modal, setModal] = useState(false);
+
   return (
-    <button type="button" className="dropdown">
-      {children}
-      <img src={DownIcon} alt="Down Icon" />
-    </button>
+    <>
+      <button
+        type="button"
+        onClick={setModal((prev) => !prev)}
+        className="dropdown">
+        {children}
+        <img src={DownIcon} alt="Down Icon" />
+      </button>
+
+      {modal && (
+        <div className="dropdown-modal">
+          <div className="display-form">
+            <label htmlFor="grouping">Grouping</label>
+            <select id="grouping" onChange={setModal((prev) => !prev)}>
+              <option value="status">Status</option>
+              <option value="priority">Priority</option>
+              <option value="user">User</option>
+            </select>
+
+            <label htmlFor="ordering">Ordering</label>
+            <select id="ordering" onChange={setModal((prev) => !prev)}>
+              <option value="priority">Priority</option>
+              <option value="sort">Sort</option>
+            </select>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
--- src/components/TasksLayout/TasksLayout.jsx
+++ src/components/TasksLayout/TasksLayout.jsx
@@ -1,29 +1,21 @@
 import Data from "../../../response.json";
 import { Card, GroupHeader } from "..";
 import "./TasksLayout.css";
-import { groupTicketsByStatus } from "../../config/groupConf";
-import { statusIcon } from "../../config/icons";
 
 export default function TasksLayout() {
-  const groupedTickets = groupTicketsByStatus(Data.tickets, "status");
-  console.log(groupedTickets);
-
   return (
     <div className="tasks-layout">
-      {Object.keys(groupedTickets)?.map((status, ind) => (
+      {/* {Object.keys(group)?.map((status, ind) => (
         <div key={ind}>
           <GroupHeader
             icon={statusIcon(status)}
             title={status}
-            length={groupedTickets[status].length}
+            length={group[status].length}
           />
-          {groupedTickets[status].map((ticket, cardInd) => (
+          {group[status].map((ticket, cardInd) => (
             <Card task={ticket} key={cardInd} />
           ))}
         </div>
-      ))}
-      {/* {Data.tickets.map((ticket, ind) => (
-        <Card task={ticket} key={ind} />
       ))} */}
     </div>
   );
